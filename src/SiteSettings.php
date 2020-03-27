c2f82e14833ac5a6cd232394caf0571309c4b255
Chris Schrijver <chris@raow.nl>
2020-03-27 23:28:19 +0100

    Make "Site Settings" translatable

--- src/SiteSettings.php
+++ src/SiteSettings.php
@@ -14,8 +14,8 @@ class SiteSettings extends AbstractSiteSettings
     {
         if (function_exists('acf_add_options_page')) {
             acf_add_options_page(array(
-                'page_title' => 'Site Settings',
-                'menu_title' => 'Site Settings',
+                'page_title' => __('Site Settings', 'offbeatwp'),
+                'menu_title' => __('Site Settings', 'offbeatwp'),
                 'menu_slug'  => self::ID,
                 'capability' => apply_filters('acf/sitesettings/capability', 'manage_options'),
                 'redirect'   => true,
